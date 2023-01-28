48a5c8a5ca47f5e3a9a2372c7f6213783cf92c96
Samuel <s.looi2@outlook.com>
2023-01-29 01:39:26 +1300

    Create ScreenCapturer class and refactored code

--- main.py
+++ main.py
@@ -16,23 +16,36 @@ FOLDER_NAME = "captures"
 # #################################
 class KeyboardHandler():
 	# Handles the storage, updating and listening of keyboard inputs
-	def __init__(self) -> None:
+	def __init__(self,callback) -> None:
 		super().__init__()
 		self.keys_down = {}
+		self.possible_str_keys = ['i', '}', 'd', 'c', 'R', '|', 'O', 'U', 'Key.alt_l', 't', ',', 'Key.f8', 'Key.right', 'T', 'b', 'm', 'Key.shift_r', 'Key.page_down', 'Key.backspace', 'l', 'Key.alt_gr', 'X', '<96>', '~', 'Key.f7', 'Key.cmd', 'W', '<255>', '^', '2', '.', 'Q', ']', '-', '6', 'H', 'Key.f1', 'y', 'D', '<105>', 'J', '`', 'P', '""', 'Key.f10', '<97>', ':', '\\x01', '1', 'K', '0', 'Key.media_play_pause', 'Key.media_volume_down', '=', 'p', 'Key.f11', 'r', 'Key.media_volume_up', 'a', '4', 'Key.tab', 'Key.page_up', '{', 'B', 'Key.media_next', 'Key.caps_lock', 'w', '/', 'Key.down', '3', 'Key.f4', 'Key.num_lock', '8', '5', '[', 'Key.f9', '9', 'Key.f6', 'N', '%', 'E', 'z', 'Key.enter', 'Key.ctrl_r', '7', 'u', 'G', '(', '*', 'Key.f3', '<12>', 'n', 'Key.space', 'g', 'q', 'Key.ctrl_l', 'h', '?', '!', '<102>', 'o', 'I', 'e', 'C', '<', '$', '<98>', 'Key.esc', ')', 'f', 'L', 'Key.home', '>', '#', '<110>', '\\x03', '<99>', 'v', 'Key.left', '@', 'M', 'Key.print_screen', ';', 'Key.insert', 'x', '+', 'Key.delete', 'Key.media_volume_mute', 'Key.shift', 'F', 'j', '<103>', '_', 'S', 's', '"', 'Key.media_previous', 'Z', 'Key.up', 'A', '<104>', 'V', '&', '<101>', '\\\\', 'Key.end', 'Key.f12', 'k', '<100>', 'Key.f2', 'Y', 'Key.f5']
+		self.callback = callback
+		self.setup_listener()
 
 	def setup_listener(self):
 		listener = keyboard.Listener(on_press=self._on_press,on_release=self._on_release)
 		listener.start()
 	
-	def _on_press(self):
-		pass
+	def _on_press(self,key):
+		self._on_event(str(key).replace("'",""),True)
 
-	def _on_release(self):
-		pass
+	def _on_release(self,key):
+		self._on_event(str(key).replace("'",""),False)
+
+	def _on_event(self,str_key:str,is_press:bool):
+		self.keys_down[str_key] = is_press
+		print("self.keys_down",self.keys_down)
+		self.update()
 
 	# This function runs whenever a keyboard input had been updated
-	def update():
-		pass
+	def update(self):
+		self.callback(self)
+
+	def get_key(self,key):
+		if key not in self.possible_str_keys:
+			raise Exception("ERROR: KeyboardHandler does not support such key `{key}`")
+		return self.keys_down[key] if key in self.keys_down else False
 
 class SelectionAreaHandler():
 	# This class handles the selection of an area
@@ -116,20 +129,48 @@ class SelectionAreaHandler():
 	def get_selection_area(self):
 		return self.selection_area
 
+
+class ScreenCapturer():
+	# This class handles the process of capturing and saving of areas of the screen 
+	def __init__(self) -> None:
+
+		# Members
+		self.capture_mode_on = False
+
+		# Setup Handlers
+		print("self.capture_selection",self.capture_selection)
+		self.selection_area_handler = SelectionAreaHandler(self.capture_selection)
+		self.keyboard_handler = KeyboardHandler(self.handleKeyboardChange)
+
+	def capture_selection(self,selection_area_handler:SelectionAreaHandler):
+		# Get selected area
+		selection_area = selection_area_handler.get_selection_area()
+		print("selection_area",selection_area)
+
+		# Capture the selected area of the screen
+		im = ImageGrab.grab(bbox=(selection_area["left"], selection_area["top"], selection_area["right"], selection_area["bottom"]))
+
+		# Save the image to a file
+		im.save("captures/screenshot.png")
+		print("took screenshot")
+
+		self.set_capture_mode(self.capture_mode_on)
+				
+
+	def handleKeyboardChange(self,keyboard_handler:KeyboardHandler):
+		key_grave_accent_down = keyboard_handler.get_key("`")
+		key_alt_l_down = keyboard_handler.get_key("Key.alt_l")
+		if key_alt_l_down and key_grave_accent_down:
+			self.set_capture_mode(not self.capture_mode_on)
+			print("self.capture_mode_on",self.capture_mode_on)
+			
+
+	def set_capture_mode(self,is_on):
+		self.capture_mode_on = is_on
 # #################################
 # FUNCTIONS
 # #################################
-def capture_selection(selection_area_handler:SelectionAreaHandler):
-	# Get selected area
-	selection_area = selection_area_handler.get_selection_area()
-	print("selection_area",selection_area)
 
-	# Capture the selected area of the screen
-	im = ImageGrab.grab(bbox=(selection_area["left"], selection_area["top"], selection_area["right"], selection_area["bottom"]))
-
-	# Save the image to a file
-	im.save("captures/screenshot.png")
-	print("took screenshot")
 
 
 
@@ -141,19 +182,26 @@ counter=0
 def setup():
 	global counter
 
+	# #################################
+	# DEPENDENCIES
+	# #################################
 	# Download manga_ocr model     !@#!@#!@#
 
 	# Create captures folder if it doesn't already exist
 	if not os.path.exists(FOLDER_NAME):
 		os.mkdir(FOLDER_NAME)
-	
-	# capture handler setup
-	selection_area_handler = SelectionAreaHandler(capture_selection)
+		
+		
+	# #################################
+	# MAIN VARIABLES
+	# #################################
+	# Screen Capturer
+	screen_capturer = ScreenCapturer()
 
 	# Main Loop
 	while True:
 		time.sleep(1)
-		counter = counter + 1
+		# counter = counter + 1
 		if counter > 3:
 			break
 

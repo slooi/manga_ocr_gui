5fa70b7612a5c5349923f95bdb74b5bf8056bad0
Samuel <s.looi2@outlook.com>
2023-01-29 18:43:32 +1300

    Create basic ScreenOverlay class

---
+++ gui/screen_overlay.py
@@ -0,0 +1,4 @@
+class ScreenOverlay():
+	# This is a GUI which overlays the screen. It renders the desktop's screen onto this GUI widget so you can click select text without highlighting the html
+	def __init__(self) -> None:
+		pass
